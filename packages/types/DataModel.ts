6e00e35a3155a4c9899d1ed85391de49b95fa4b7
rn404 <nekonekonekona@gmail.com>
2026-02-06 23:08:09 +0900

    feat(types): add AttributeOf utility type

--- packages/types/DataModel.ts
+++ packages/types/DataModel.ts
@@ -10,4 +10,15 @@ type DataModel<T extends object,> =
   & RequiredDataModel
   & Omit<T, keyof RequiredDataModel>
 
-export type { DataModel, }
+/**
+ * DataModel から属性部分（T）を抽出する
+ *
+ * @example
+ * ```ts
+ * type CoinAttributes = AttributeOf<CoinDataModel>
+ * // => { userId: string, familyId: string, coinTypeId: string, amount: number }
+ * ```
+ */
+type AttributeOf<T,> = T extends DataModel<infer U> ? U : never
+
+export type { AttributeOf, DataModel, }
