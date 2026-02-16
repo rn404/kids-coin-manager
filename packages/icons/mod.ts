f8bd868cb3ee77d5435dbce27c4306442e010440
rn404 <nekonekonekona@gmail.com>
2026-02-16 19:45:24 +0900

    feat(icons): add IconEdit component

--- packages/icons/mod.ts
+++ packages/icons/mod.ts
@@ -1,10 +1,12 @@
 import { IconCoin, } from './IconCoin.tsx'
+import { IconEdit, } from './IconEdit.tsx'
 
 const icons = {
   Coin: IconCoin,
+  Edit: IconEdit,
 } as const
 
 type IconName = keyof typeof icons
 
-export { IconCoin, icons, }
+export { IconCoin, IconEdit, icons, }
 export type { IconName, }
