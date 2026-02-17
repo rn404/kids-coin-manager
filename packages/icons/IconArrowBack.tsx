6e67d0ddcde183fdf594cfb682b17c3f55cf92ee
rn404 <nekonekonekona@gmail.com>
2026-02-17 14:13:05 +0900

    feat(icons): add ArrowBack, Check, and TrashX icons

---
+++ packages/icons/IconArrowBack.tsx
@@ -0,0 +1,22 @@
+// Icon from Tabler Icons (MIT License) https://tabler.io/icons
+export function IconArrowBack(
+  props: { size?: number | string; class?: string },
+) {
+  return (
+    <svg
+      width={props.size}
+      height={props.size}
+      class={props.class}
+      xmlns='http://www.w3.org/2000/svg'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      stroke-width='2'
+      stroke-linecap='round'
+      stroke-linejoin='round'
+    >
+      <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+      <path d='M9 11l-4 4l4 4m-4 -4h11a4 4 0 0 0 0 -8h-1' />
+    </svg>
+  )
+}
--- packages/icons/mod.ts
+++ packages/icons/mod.ts
@@ -1,12 +1,18 @@
 import { IconCoin, } from './IconCoin.tsx'
 import { IconEdit, } from './IconEdit.tsx'
+import { IconArrowBack, } from './IconArrowBack.tsx'
+import { IconCheck, } from './IconCheck.tsx'
+import { IconTrashX, } from './IconTrashX.tsx'
 
 const icons = {
   Coin: IconCoin,
   Edit: IconEdit,
+  ArrowBack: IconArrowBack,
+  Check: IconCheck,
+  TrashX: IconTrashX,
 } as const
 
 type IconName = keyof typeof icons
 
-export { IconCoin, IconEdit, icons, }
+export { IconArrowBack, IconCheck, IconCoin, IconEdit, icons, IconTrashX, }
 export type { IconName, }
