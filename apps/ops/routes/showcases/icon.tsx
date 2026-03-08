176675400567a5d20ef1c20219fd6262feaa2fe5
rn404 <nekonekonekona@gmail.com>
2026-03-09 00:24:46 +0900

    feat(ops): add UI component showcases section

---
+++ apps/ops/routes/showcases/icon.tsx
@@ -0,0 +1,16 @@
+import { page, } from 'fresh'
+import { define, } from '../../main.ts'
+import { IconExamplePage, } from '@workspace/ui'
+
+export const handler = define.handlers({
+  GET(_ctx,) {
+    return page({},)
+  },
+},)
+
+const IconShowcase = define.page<typeof handler>(() => {
+  return <IconExamplePage />
+},)
+
+// deno-lint-ignore internal/no-default-export
+export default IconShowcase
