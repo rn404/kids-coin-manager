f3940a4e8916fa9eed26ac5a7e167cc0fff0d7d0
rn404 <nekonekonekona@gmail.com>
2026-03-09 00:05:08 +0900

    style(ui): use flex gap for ExamplePage header spacing

--- packages/ui/examples/ExamplePage.tsx
+++ packages/ui/examples/ExamplePage.tsx
@@ -7,8 +7,8 @@ function ExamplePage({ title, description, children, }: {
 },) {
   return (
     <div>
-      <div class='p-4 bg-neutral-200 rounded mb-6'>
-        <h1 class='mb-2 text-4xl font-bold'>{title}</h1>
+      <div class='p-4 bg-neutral-200 rounded mb-6 flex flex-col gap-2'>
+        <h1 class='text-4xl font-bold'>{title}</h1>
         {description && <div>{description}</div>}
       </div>
       <div class='relative'>
