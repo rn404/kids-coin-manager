76cf379d22a4f8f753855b96e1134ec80a6bd7f9
rn404 <nekonekonekona@gmail.com>
2026-03-08 23:42:33 +0900

    feat(ui): add Link component styling and external link support

---
+++ packages/ui/examples/Link.tsx
@@ -0,0 +1,18 @@
+import { ExamplePage, } from './ExamplePage.tsx'
+import { ExampleSection, } from './ExampleSection.tsx'
+import { Link, } from '../components/Link.tsx'
+
+const LinkExamplePage = () => {
+  return (
+    <ExamplePage title='Link'>
+      <ExampleSection title='Default'>
+        <Link href='#'>リンクテキスト</Link>
+      </ExampleSection>
+      <ExampleSection title='External'>
+        <Link href='#' externalLink>外部リンク</Link>
+      </ExampleSection>
+    </ExamplePage>
+  )
+}
+
+export { LinkExamplePage, }
